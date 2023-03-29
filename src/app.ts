71e859a6bc3b317157d69675e4de5b286589323c
Luan Oliveira <luanoliveira17@hotmail.com>
2023-03-29 19:01:19 -0300

    feat: add env vars; add eslint config

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-import fastify from "fastify";
-import cookie from "@fastify/cookie"
+import fastify from 'fastify'
+import cookie from '@fastify/cookie'
 
 // TODO import routes
 
-export const app = fastify();
+export const app = fastify()
 
 app.register(cookie)
 
